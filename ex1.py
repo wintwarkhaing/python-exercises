f6b170ea5fbffe04c5ba35839d29b508c1994755
wintwarkhaing <wwkhaing777@gmail.com>
2018-06-10 20:58:50 -0700

    execise 1

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print("Hello World")
+print("Hello Again")
+print("I like playing type this ")
+print("This is fun")
+print("Yay!Printing")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this')
