b31bd5861f6ee0a4a964634304ee24a54491bfa0
wintwarkhaing <wwkhaing777@gmail.com>
2018-06-13 20:49:18 -0700

    exercise 34

---
+++ ex34.py
@@ -0,0 +1,12 @@
+animals=['bear', 'python3.7', 'peacock', 'kangaroo', 'whale', 'platpus']
+
+print(f"The animal at 1 is the 2nd animal and is a {animals[1]}.")
+print(f"The third (3rd) animal is at 2 and is a {animals[2]}.")
+print(f"The first (1st) animal is at 0 and is a {animals[0]}.")
+print(f"The animal at 3 is the 4th animal and is a {animals[3]}.")
+print(f"The fifth (5th) animal is at 4 and is a {animals[4]}.")
+print(f"The animal at 2 is the 3rd animal and is a {animals[2]}.")
+print(f"The sisth (6th) animal is at 5 and is a {animals[5]}.")
+print(f"The animal at 4 is the 5th animal and is a {animals[4]}.")
+
+
