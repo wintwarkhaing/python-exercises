f60db6b869225913d43c1a054444c126bafc7b6e
wintwarkhaing <wwkhaing777@gmail.com>
2018-06-25 20:57:39 -0700

    add ex101.py

--- ex101.py
+++ ex101.py
@@ -1,9 +1,9 @@
-tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
-backslash_cat = "I'm \\ a \\ cat."
-fat_cat = """I'll do a list:\t* Cat food\t*Fishies\t*Catnip\n\t*Grass"""
-print(tabby_cat)
-print(persion_cat)
-print(backlash_cat)
-print(fat_cat)
+tabby_dog = "\tI'm tabbed in."
+persian_dog = "I'm split\non a line."
+backslash_dog = "I'm \\ a \\ cat."
+fat_dog = """I'll do a list:\t* Cat food\t*Fishies\t*Catnip\n\t*Grass"""
+print(tabby_dog)
+print(persion_dog)
+print(backlash_dog)
+print(fat_dog)
 
