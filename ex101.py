a7a82b89a2233450932082373a9e7dfbc72f5002
wintwarkhaing <wwkhaing777@gmail.com>
2018-06-25 21:01:23 -0700

    add ex101.py

--- ex101.py
+++ ex101.py
@@ -4,6 +4,6 @@ backslash_dog = "I'm \\ a \\ cat."
 fat_dog = """I'll do a list:\t* Cat food\t*Fishies\t*Catnip\n\t*Grass"""
 print(tabby_dog)
 print(persian_dog)
-print(backlash_dog)
+print(backslash_dog)
 print(fat_dog)
 
