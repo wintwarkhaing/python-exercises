296eb694ccdc563a9b9fd0713a58e6eaeadd02ce
wintwarkhaing <wwkhaing777@gmail.com>
2018-06-25 20:58:55 -0700

    add ex101.py

--- ex101.py
+++ ex101.py
@@ -3,7 +3,7 @@ persian_dog = "I'm split\non a line."
 backslash_dog = "I'm \\ a \\ cat."
 fat_dog = """I'll do a list:\t* Cat food\t*Fishies\t*Catnip\n\t*Grass"""
 print(tabby_dog)
-print(persion_dog)
+print(persian_dog)
 print(backlash_dog)
 print(fat_dog)
 
