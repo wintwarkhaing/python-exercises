70ac500f04154f0794a0afdf3b773a56d8c9e794
wintwarkhaing <39288985+wintwarkhaing@users.noreply.github.com>
2018-06-25 21:05:28 -0700

    Update ex101.py
    Change dog to rat

--- ex101.py
+++ ex101.py
@@ -1,9 +1,9 @@
-tabby_dog = "\tI'm tabbed in."
-persian_dog = "I'm split\non a line."
-backslash_dog = "I'm \\ a \\ cat."
-fat_dog = """I'll do a list:\t* Cat food\t*Fishies\t*Catnip\n\t*Grass"""
-print(tabby_dog)
-print(persian_dog)
-print(backslash_dog)
-print(fat_dog)
+tabby_rat = "\tI'm tabbed in."
+persian_rat = "I'm split\non a line."
+backslash_rat = "I'm \\ a \\ rat."
+fat_rat = """I'll do a list:\t* Cat food\t*Fishies\t*Catnip\n\t*Grass"""
+print(tabby_rat)
+print(persian_rat)
+print(backslash_rat)
+print(fat_rat)
 
