c822622e9eabe5bc1fd8e07ff090e713cf7e9625
wintwarkhaing <39288985+wintwarkhaing@users.noreply.github.com>
2018-06-25 21:15:11 -0700

    Delete ex101.py

--- ex101.py
+++
@@ -1,9 +0,0 @@
-tabby_rat = "\tI'm tabbed in."
-persian_rat = "I'm split\non a line."
-backslash_rat = "I'm \\ a \\ rat."
-fat_rat = """I'll do a list:\t* Cat food\t*Fishies\t*Catnip\n\t*Grass"""
-print(tabby_rat)
-print(persian_rat)
-print(backslash_rat)
-print(fat_rat)
-
